a11bfbd93cb1f9cd8f71520d7a6f1d435aa03a50
Tux <clark@dev-engine.net>
2018-03-12 19:11:13 +0800

    use index 0 on mx

--- test.php
+++ test.php
@@ -19,7 +19,7 @@ $lookup = new \EmailChecker\Lookup ($host);
 // get mx records
 $mx = $lookup->getMxRecords();
 // telnet
-$telnet = new \EmailChecker\Telnet($mx[1]);
+$telnet = new \EmailChecker\Telnet($mx[0]);
 // say helo
 $helo = $telnet->sayHelo();
 // set mail from
